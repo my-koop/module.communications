24121c59792e5d4d42281bd2ddc082a98b77c86f
Cellule <mike.ferris@hotmail.com>
2014-11-18 15:46:36 -0500

    Interface to send email with SES

--- lib/module.ts
+++ lib/module.ts
@@ -1,11 +1,34 @@
 import utils = require("mykoop-utils");
+var logger = utils.getLogger(module);
+var ses = require('node-ses');
 
 var ApplicationError = utils.errors.ApplicationError;
 
 class Module extends utils.BaseModule implements mkcommunications.Module {
-  init() {
+  ses = null;
 
+  init() {
+    var sesConfig;
+    try{
+      sesConfig = require("sesConfig.json");
+      this.ses = ses.createClient({
+        key: sesConfig.key,
+        secret: sesConfig.secret,
+        amazon: sesConfig.host
+      });
+    } catch(e) {
+      console.error("Unable to find SES configuration [sesConfig.json]", e);
+    }
   }
+
+  sendEmail(params: mkcommunications.SendEmailParams, callback) {
+    if(this.ses) {
+      this.ses.sendemail(params, function(err, data, res) {
+        callback(err);
+      });
+    }
+  }
+
 }
 
 export = Module;
